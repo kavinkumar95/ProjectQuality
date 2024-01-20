35d54937349081e6521f3ca34ff67f5b32ecbcbd
kavmuthusamy <mkavinkumar@outlook.com>
2024-01-20 12:36:26 +0530

    Update start_service.sh

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Set the path to your docker-compose.yml file
-docker_compose_file="docker-compose.yml"
-
-
 # Start the Docker containers using Docker Compose
-docker-compose up -d
+
+docker-compose -f docker-compose.yaml up -d
 
 # Check if the containers are running
 if [ $? -eq 0 ]; then
