53a28cbe2ce56b2ae71589d3a4980097759be8d5
kavmuthusamy <mkavinkumar@outlook.com>
2024-01-20 12:56:53 +0530

    code commit

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -7,6 +7,8 @@ docker-compose -f docker-compose.yaml up -d
 # Check if the containers are running
 if [ $? -eq 0 ]; then
     echo "Docker containers started successfully!"
+    sleep 10
+    bash create_excel.sh
 else
     echo "Failed to start Docker containers. Please check the Docker Compose configuration and try again."
 fi
